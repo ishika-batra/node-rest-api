8f50f7e78190b3c2bde3f53f5f35ac1a85c1c442
ishika2203 <ishikabatra.net@gmail.com>
2023-10-03 14:33:28 +0530

    schema created

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+const Post = require('../models/Post');
+
 router.get('/', (req, res) => {
   res.send('We are on posts');
+});
+
+router.get('/specific', (req, res) => {
+  res.send('Specific post.');
 });
 
 module.exports = router;
