5457791150af62b2ff66edef9bc6410d5b153064
ishika2203 <ishikabatra.net@gmail.com>
2023-10-03 16:39:31 +0530

    post request made

--- routes/posts.js
+++ routes/posts.js
@@ -8,8 +8,21 @@ router.get('/', (req, res) => {
   res.send('We are on posts');
 });
 
-router.get('/specific', (req, res) => {
-  res.send('Specific post.');
+router.post('/', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .exec()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json;
+    });
 });
 
 module.exports = router;
